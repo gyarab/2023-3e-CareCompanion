f01e6e8e2210a8af56c4d29e5262777d3414432e
Rumburaq3 <hurt.oliver7@gmail.com>
2023-12-09 14:34:34 +0100

    sign in and log in working

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -62,11 +62,29 @@ class MainActivity : AppCompatActivity() {
             // Successfully signed in
             val user = FirebaseAuth.getInstance().currentUser
             // ...
+            /*
+            val user = Firebase.auth.currentUser
+            user?.let {
+                // Name, email address, and profile photo Url
+                val name = it.displayName
+                val email = it.email
+                val photoUrl = it.photoUrl
+
+                // Check if user's email is verified
+                val emailVerified = it.isEmailVerified
+
+                // The user's ID, unique to the Firebase project. Do NOT use this value to
+                // authenticate with your backend server, if you have one. Use
+                // FirebaseUser.getIdToken() instead.
+                val uid = it.uid
+            }
+
+             */
             val intent = Intent(this, LoginActivity::class.java).apply {
                 putExtra("name", binding.editTextText.text.toString())
                 putExtra("number", binding.editTextNumber.text.toString())
                 putExtra("checked", binding.checkBox.isChecked)
-                putExtra("user", user.toString() )
+                putExtra("user", user)
             }
             startActivity(intent)
             binding.textView2.text = user.toString()
@@ -85,9 +103,20 @@ class MainActivity : AppCompatActivity() {
 
         // Get an instance of AuthUI based on the default app
         // Get an instance of AuthUI based on the default app
-        val signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setTheme(R.style.GreenTheme).build()
 
-        signInLauncher.launch(signInIntent)
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            // already signed in
+            binding.button3.text = "alerady signed id"
+            binding.textView2.text = auth.currentUser.toString()
+        } else {
+            // not signed in
+            val signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setTheme(R.style.GreenTheme).build()
+
+            signInLauncher.launch(signInIntent)
+        }
+
+
     }
 
 
