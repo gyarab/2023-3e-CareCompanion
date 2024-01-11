fed4c3e08e42e9f6f1efec7287d2aa82825dc3c0
sabi <sabina.javurkova@student.gyarab.cz>
2024-01-11 18:45:03 +0100

    registrace done

--- login/urls.py
+++ login/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index_login'),
     path('/help', views.help_w_registration, name='help'),
     path('/odhlaseni', views.logout_user, name='logout'),
+    path('/registrace', views.register_user, name='registration'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import RegisterUserForm
 
 
 def index(request):
-
     if request.method == 'POST':
         username = request.POST.get("username", "")
         password = request.POST.get("password", "")
@@ -15,7 +17,8 @@ def index(request):
             login(request, user)
             return redirect("/opatrovnik")
         else:
-            messages.success(request, 'Při přihlašování nastala chyba, znova si zkontrolujte zadané údaje, případně se obraťte na IT podporu. ')
+            messages.success(request,
+                             'Při přihlašování nastala chyba, znova si zkontrolujte zadané údaje, případně se obraťte na IT podporu. ')
             return redirect("/prihlaseni")
 
 
@@ -27,6 +30,24 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'Odhlášení proběhlo úspěšně')
     return redirect('index_login')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registrace proběhla úspěšně')
+            return redirect('/opatrovnik')
+
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'register.html', {'form': form})
 
 
 def help_w_registration(request):
