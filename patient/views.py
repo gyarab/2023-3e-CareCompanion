090f5c4a9794d9937bfc30165567e53ba3dbaf62
sabi <sabina.javurkova@student.gyarab.cz>
2024-03-15 16:22:11 +0100

    zobrazeni opatrovniku a jejich smen pro klienta DONE

--- patient/views.py
+++ patient/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from babel.dates import format_date
 
 from caregiver.models import Caregiver
 from .decorators import patient_required
@@ -10,44 +11,49 @@ def index(request):
     return render(request, 'index_patient.html')
 
 
-# TODO: kdyz opatrovnik nema upcoming shift aby to napsalo neco ne jenom prazdny radek
 @patient_required
 def caregivers_list(request):
     now = datetime.now()
-
     users_shift_info = []
+
     for caregiver in Caregiver.objects.prefetch_related('shift_set'):
-        shifts_today = caregiver.shift_set.filter(date_of_shift=now.date())
-        if shifts_today.exists():
-            shift = shifts_today.first()
-            shift_start = datetime.combine(now.date(), shift.start)
-            shift_end = datetime.combine(now.date(), shift.end)
-            on_shift = shift_start <= now <= shift_end
+
+        # Saves the first upcoming shift (either today or in the future)
+        next_shift = caregiver.shift_set.filter(
+            date_of_shift__gte=now.date(),
+            end__gt=now.time()  # Excludes shifts that have already ended
+        ).order_by('date_of_shift', 'start').first()
+
+        if next_shift:
+            shift_start = datetime.combine(next_shift.date_of_shift, next_shift.start)
+            shift_end = datetime.combine(next_shift.date_of_shift, next_shift.end)
+            if shift_start <= now <= shift_end:
+                # If the shift has already started but not ended, the caregiver is on shift
+                users_shift_info.append({
+                    'first_name': caregiver.first_name,
+                    'last_name': caregiver.surname,
+                    'on_shift': True,
+                    'shift_end': next_shift.end,
+                })
+            else:
+                # If the shift is upcoming, displaying its details
+                next_shift_date = format_date(next_shift.date_of_shift, format='EEEE d. MMMM', locale='cs_CZ')
+                users_shift_info.append({
+                    'first_name': caregiver.first_name,
+                    'last_name': caregiver.surname,
+                    'on_shift': False,
+                    'next_shift_date': next_shift_date,
+                    'next_shift_start': next_shift.start,
+                })
+        else:
+            # If there are no upcoming shifts
             users_shift_info.append({
                 'first_name': caregiver.first_name,
                 'last_name': caregiver.surname,
-                'on_shift': on_shift,
-                'shift_end': shift.end if on_shift else None,
+                'on_shift': False,
+                'next_shift_date': '',
+                'next_shift_start': 'Zadne registrovane budouci smeny',
             })
-        else:
-            next_shift = caregiver.shift_set.filter(date_of_shift__gt=now.date()).order_by('date_of_shift',
-                                                                                           'start').first()
-            if next_shift is not None:
-                users_shift_info.append({
-                    'first_name': caregiver.first_name,
-                    'last_name': caregiver.surname,
-                    'on_shift': False,
-                    'next_shift_date': next_shift.date_of_shift,
-                    'next_shift_start': next_shift.start,
-                })
-            else:
-                users_shift_info.append({
-                    'first_name': caregiver.first_name,
-                    'last_name': caregiver.surname,
-                    'on_shift': False,
-                    'next_shift_date': 'no upcoming shifts',
-                    'next_shift_start': '',
-                })
 
     return render(request, 'caregivers_list.html', {'users_shift_info': users_shift_info})
 
