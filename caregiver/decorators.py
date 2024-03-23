fcb1a41a4a4e7f71d0c90b64fb9fe0b290553aea
sabi <sabina.javurkova@student.gyarab.cz>
2024-03-23 22:25:28 +0100

    smazani zbytecnych messages a redirects

--- caregiver/decorators.py
+++ caregiver/decorators.py
@@ -4,23 +4,11 @@ from django.shortcuts import redirect
 
 def caregiver_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
-
         if request.user.is_authenticated:
             groups = request.user.groups.values_list('name', flat=True)
-
             if 'Caregivers' in groups:
                 return view_func(request, *args, **kwargs)
-            elif 'Patients' in groups:
-                messages.success(request, 'Na tuto část webu nemáte přístup.')
-                return redirect('index_patient')
-            elif 'Admins' in groups:
-                messages.success(request, 'Na tuto část webu nemáte přístup.')
-                return redirect('administration')
-            else:
-                messages.success(request, 'Obraťte se IT podporu.')
-                return redirect('login_user')
-        else:
-            messages.success(request, 'Pro přístup do aplikace se přihlašte.')
-            return redirect('login_user')
 
+        messages.success(request, 'Pro přístup do aplikace se přihlašte.')
+        return redirect('login_user')
     return _wrapped_view
--- patient/decorators.py
+++ patient/decorators.py
@@ -4,24 +4,11 @@ from django.shortcuts import redirect
 
 def patient_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
-
         if request.user.is_authenticated:
             groups = request.user.groups.values_list('name', flat=True)
-
             if 'Patients' in groups:
                 return view_func(request, *args, **kwargs)
-            elif 'Admins' in groups:
-                messages.success(request, 'Na tuto část webu nemáte stupid.')
-                return redirect('administration')
-            elif 'Caregivers' in groups:
-                messages.success(request, 'Na tuto část webu nemáte přístup.')
-                return redirect('index_caregiver')
-            else:
-                messages.success(request, 'Obraťte se IT podporu.')
-                return redirect('login_user')
 
-        else:
-            messages.success(request, 'Pro přístup do aplikace se přihlašte.')
-            return redirect('login_user')
-
+        messages.success(request, 'Pro přístup do aplikace se přihlašte.')
+        return redirect('login_user')
     return _wrapped_view
