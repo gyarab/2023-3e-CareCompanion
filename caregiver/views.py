0fdeda8ccd60831c47a6103892b4cfbba97af223
sabi <sabina.javurkova@student.gyarab.cz>
2024-04-03 15:33:16 +0200

    Funkcni rozvrh smeny pro opatrovnika. Zobrazuje dnesni smenu nebo tu nejdrivejsi + napln smeny

--- caregiver/views.py
+++ caregiver/views.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from babel.dates import format_date
 
+from django.db.models import Q
 from django.shortcuts import render
 
 from .decorators import caregiver_required
@@ -31,20 +33,45 @@ def patient_info(request, full_name_of_patient):
 def shift_schedule(request):
     # caregivers = Caregiver.objects.all()
     caregiver = request.user.caregiver_profile
-    current_date_time = datetime.now()
-    month = datetime.today().month
-    day = datetime.today().day
-    year = datetime.today().year
-    time = datetime.now().time()
+    now = datetime.now()
 
-    context = {
-        'caregiver': caregiver,
-        'current_month': month,
-        'current_day': day,
-        'current_year': year,
-        'current_time': time,
-        'current_date_time': current_date_time
-    }
+    # Saves the first upcoming/current shift (either today or in the future)
+    next_shift = caregiver.shift_set.filter(
+        Q(date_of_shift__gt=now.date()) |  # Future shifts OR
+        (Q(date_of_shift=now.date()) & Q(end__gt=now.time()))  # Today's shifts that have not ended
+    ).order_by('date_of_shift', 'start').first()
+
+    if next_shift:
+        shift_start = datetime.combine(next_shift.date_of_shift, next_shift.start)
+        shift_end = datetime.combine(next_shift.date_of_shift, next_shift.end)
+        if shift_start <= now <= shift_end:
+
+            # If the shift has already started but not ended, the caregiver is on shift
+            context = {
+                'caregiver': caregiver,
+                'on_shift': True,
+                'shift_start': shift_start.time,
+                'shift_end': shift_end.time,
+                'activities':  next_shift.activity_set.all
+            }
+        else:
+            # If the shift is upcoming, displaying its details
+            next_shift_date = format_date(next_shift.date_of_shift, format='EEEE d. MMMM', locale='cs_CZ')
+            context = {
+                'caregiver': caregiver,
+                'on_shift': False,
+                'next_shift_date': next_shift_date,
+                'next_shift_start': next_shift.start,
+                'activities': next_shift.activity_set.all
+            }
+    else:
+        context = {
+            'caregiver': caregiver,
+            'on_shift': False,
+            'next_shift_date': False
+        }
+
+    context.update({})
     return render(request, 'shift_schedule.html', context)
 
 
