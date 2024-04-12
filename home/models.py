ab7b55dbff64afd44efbbf68cced220677055ce7
sabi <sabina.javurkova@student.gyarab.cz>
2024-04-13 01:23:46 +0200

    vytvoren zaklad pro uvodni stranku

--- home/models.py
+++ home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.IntegerField(null=True)
+    email = models.EmailField(max_length=50)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    zip_code = models.IntegerField(null=True)
+    city = models.CharField(max_length=50)
+
+
+class DaySchedule(models.Model):
+    time = models.TimeField()
+    description = models.CharField(max_length=50)
+
+
+class Announcement(models.Model):
+    text = models.TextField()
+    delete_when = models.DateTimeField()
